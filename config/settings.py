6622bb53cffb351ca78ed3b8c08fb125d8a32e1b
cnboker <coobots@gmail.com>
2024-10-31 08:57:56 +0800

    remove config from tracking

--- config/settings.py
+++
@@ -1,7 +0,0 @@
-import vosk
-import os
-
-api_key = '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'
-
-sample_rate = 44100
-vosk_model = vosk.Model(os.path.expanduser("~") + "/code/voice_chatgpt/.venv/vosk-model-cn-0.22")
